05144107d14d6b466c43c617a7d080d6747e7338
pixelguy13 <13hritrock@gmail.com>
2026-06-17 19:30:25 +0545

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="px-8 py-4 bg-gray-200 text-black text-center">
+      <p>© 2023 Hritik Yadav | KUSMS, Nepal</p>
+    </footer>
+  );
+};
+
+export default Footer;
